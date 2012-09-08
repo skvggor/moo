2033f9cb6cb25d0f5dd22b4387726c08d99063b5
Marcos Garcia de Lima <marckfree@gmail.com>
2012-09-08 16:09:16 -0300

    Utilizando Morph

--- js/scripts.js
+++ js/scripts.js
@@ -4,9 +4,28 @@ window.addEvent('domready', function() {
     text = element.get('text');
     i = 0;
 
-    element.addEvent('click', function(){
-        i += 1;
-        this.empty();
-        this.set('text', 'click{' + i + ', ' + text + '}');
+    element.morph({
+        'text-align': 'center',
+        'padding':'10',
+        'width': '200'
     });
+
+    element.addEvent('mouseover', function(event){
+        element.morph({
+           'color': '#FFF',
+           'background-color': '#000' 
+        });
+        
+        // i += 1;
+        // this.empty();
+        // this.set('text', i < 10 ? 'click{0' + i + ', ' + text + '}' : 'click{' + i + ', ' + text + '}');
+    });
+
+    element.addEvent('mouseout', function() {
+        element.morph({
+            'color': '#000',
+            'background-color': '#FFF'
+        });
+    });
+
 });
